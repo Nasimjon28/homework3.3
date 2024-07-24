322df39ac225f9d27ca2ca127be5725e178b05f7
Nasimjon28 <shahlo.reg@gmail.com>
2024-07-24 10:02:04 +0300

    dhgfчарр

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-def print_params(a = 1, b = 'строка', c = True):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
+print_params()
+print_params(b=25)
+print_params(c=[1,2,3])
 
-values_list = [2.2, True, [1, 2, 3]]
-values_dict = {'a': 74, 'b': "Ангарск", 'c': 6.5}
+values_list = [1, 'вторая строка', False]
+values_dict = {'a': 2, 'b': 'book', 'c': True}
 
-values_list_2 = [54.32, 'Строка']
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка' ]
 print_params(*values_list_2, 42)
+
+
